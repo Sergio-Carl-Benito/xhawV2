9c25d02f536a31c5e115e310899c8fc332ab389e
ST10459382-Sergio-Benito <163974756+Sergio-Carl-Benito@users.noreply.github.com>
2024-09-19 21:16:43 +0200

    added read me

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-// App.tsx
+// App.tsx file
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
